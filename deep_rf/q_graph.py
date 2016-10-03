cd55cecc606b63ff738d7b1e329ead750e414837
kyleclo <kyleclo@uw.edu>
2016-10-03 13:21:38 +0800

    refactored QGraph class to take kwargs

--- deep_rf/q_graph.py
+++ deep_rf/q_graph.py
@@ -3,50 +3,108 @@ import _utils
 
 
 class QGraph(object):
-    """
-        q_input: (tf.placeholder float [None, board_height, board_width, num_frames]) - tf placeholder for state
-        q_output: (tf.Tensor of action_values [None, num_actions]) - Q function output to evaluated with tf.run()
+    """Stores input and output Tensors of Graph representing Q-function.
+
+    QGraph constructor takes two sets of parameters:
+        1. If defining own Graph structure, provide q_input and q_output
+        2. If using predefined Graph, provide name and necessary inputs
+
+    Parameters
+    ----------
+    q_input : tf.placeholder, shape = [None, frame_height, frame_width, num_frames]
+        Tensor that holds a batch_size of input game States for training.
+
+    q_output: tf.Tensor, shape = [None, num_actions]
+        Tensor of batch_size output vectors containing Q-values for each action.
+
+    name : str, 'snake_default'
+        Specify the name of a predefined Tensorflow Graph structure.
+
+    frame_height : int
+        For predefined Graph
+
+    frame_width : int
+        For predefined Graph
+
+    num_frames : int
+        For predefined Graph
+
+    num_actions: int
+        For predefined Graph
+
+
+    Attributes
+    ----------
+    q_input : tf.placeholder
+        See above.
+
+    q_output : tf.Tensor
+        See above.
+
+    graph : tf.Graph
+        Tensorflow graph corresponding to q_input.
+
+    var_list : list of str
+        Contains names of trainable variables in Graph.
 
     """
-    def __init__(self, q_input, q_output):
-        self.q_input = q_input
-        self.q_output = q_output
+
+    def __init__(self, **kwargs):
+        if 'name' in kwargs:
+            name = kwargs['name']
+            frame_height = kwargs['frame_height']
+            frame_width = kwargs['frame_width']
+            num_frames = kwargs['num_frames']
+            num_actions = kwargs['num_actions']
+
+            if name == 'snake_default':
+                self.q_input, self.q_output = QGraph.create_snake_default(
+                    frame_height, frame_width, num_frames, num_actions)
+            else:
+                raise Exception('No predefined QGraph with this name.')
+
+        elif 'q_input' in kwargs and 'q_output' in kwargs:
+            self.q_input = kwargs['q_input']
+            self.q_output = kwargs['q_output']
+
+        else:
+            raise Exception(
+                'Either provide q_input & q_output or choose a predefined Graph.')
+
         self.graph = self.q_input.graph
-        self.var_list = self.graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
-
+        self.var_list = self.graph.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES)
 
     @staticmethod
-    def default_q_graph(game, num_frames):
-        """ initialize Q function input & output
-
-            Returns:
-                QGraph: a q graph
-        """
-
+    def create_snake_default(frame_height, frame_width, num_frames,
+                             num_actions):
+        """3 conv + 2 fc layers. Using 16->8->8 2x2 kernels."""
         g = tf.Graph()
 
         with g.as_default():
-
             # input layer
             q_input = tf.placeholder(dtype=tf.float32,
-                                     shape=[None, game.frame_height,
-                                            game.frame_width, num_frames])
+                                     shape=[None, frame_height,
+                                            frame_width, num_frames])
 
             # hidden convolutional layer 1
             w_conv1 = _utils.init_conv_filter(filter_height=2,
                                               filter_width=2,
-                                              in_channels=int(q_input.get_shape()[-1]),
+                                              in_channels=int(
+                                                  q_input.get_shape()[-1]),
                                               out_channels=16)
             h_conv1 = tf.nn.relu(_utils.conv2d(input=q_input,
                                                filter=w_conv1,
                                                stride=1))
 
             # hidden convolutional layer 2
-            w_conv2 = _utils.init_conv_filter(1, 1, int(w_conv1.get_shape()[-1]), 8)
+            w_conv2 = _utils.init_conv_filter(1, 1,
+                                              int(w_conv1.get_shape()[-1]), 8)
             h_conv2 = tf.nn.relu(_utils.conv2d(h_conv1, w_conv2, 1))
 
             # hidden convolutional layer 3
-            w_conv3 = _utils.init_conv_filter(1, 1, int(w_conv2.get_shape()[-1]), 8)
+            w_conv3 = _utils.init_conv_filter(1, 1,
+                                              int(w_conv2.get_shape()[-1]), 8)
             h_conv3 = tf.nn.relu(_utils.conv2d(h_conv2, w_conv3, 1))
 
             # reshape convolutional layer from 4D -> 2D tensor
@@ -61,8 +119,8 @@ class QGraph(object):
 
             # output = fully connected layer 2
             w_fc2 = _utils.init_fc_weights(height=len_h_conv3,
-                                           width=len(game.action_list))
-            b_fc2 = _utils.init_fc_bias(length=len(game.action_list))
+                                           width=num_actions)
+            b_fc2 = _utils.init_fc_bias(length=num_actions)
             q_output = tf.matmul(h_fc1, w_fc2) + b_fc2
 
-        return QGraph(q_input, q_output)
+        return q_input, q_output
--- main.py
+++ main.py
@@ -13,28 +13,33 @@ frame_width = 4
 num_frames = 2
 
 my_game = snake.SnakeGame(board_height=frame_height, board_width=frame_width)
-my_q_graph = rf.QGraph.default_q_graph(my_game, num_frames=num_frames)
+my_q_graph = rf.QGraph(name='snake_default', frame_height=frame_height,
+                       frame_width=frame_width, num_frames=num_frames,
+                       num_actions=len(my_game.action_list))
+
 
 def my_reward(params):
     return params['new_score'] - params['last_score'] + \
            (-1.0 if params['is_game_over'] else 0.0) - .001
 
+
 my_rf = rf.DeepRFLearner(my_game, my_q_graph, my_reward)
 
 
 def play_one_game(deep_rf_learner):
-
     game = snake.SnakeGame(frame_height, frame_width)
     first_frame = game.get_frame()
     state_padding = [np.zeros(first_frame.shape) for _ in
                      range(num_frames - 1)]
-    current_state = rf.State(frames_tuple=tuple(state_padding) + (first_frame,))
+    current_state = rf.State(
+        frames_tuple=tuple(state_padding) + (first_frame,))
 
     def print_frame_and_get_action(state):
         print "\n" * 20 + str(game)
         print "\nQ-val with actions: " + \
-            str(dict(zip(game.action_list,
-                np.round(deep_rf_learner.evaluate_q_function(state=state), 3))))
+              str(dict(zip(game.action_list,
+                           np.round(deep_rf_learner.evaluate_q_function(
+                               state=state), 3))))
         action = deep_rf_learner.choose_action(state=state)
         print "Next Action: " + action
         return action
@@ -59,4 +64,3 @@ while True:
                            batch_size=100,
                            num_training_steps=100)
     play_one_game(my_rf)
-
